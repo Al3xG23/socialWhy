580c57cec5ecde950f437b8320e32d056f8a616a
Onyx Vega <mabs.24gl@gmail.com>
2024-05-04 19:48:21 -0400

    added more TODOs

---
+++ utils/data.js
@@ -0,0 +1,20 @@
+TODO
+const users = [
+    // example data
+    {
+        "username": "lernantino",
+        "email": "lernantino@gmail.com"
+      }
+];
+const thoughts = [
+    // example data
+    {
+        "thoughtText": "Here's a cool thought...",
+        "username": "lernantino",
+        "userId": "5edff358a0fcb779aa7b118b"
+      }
+];
+
+// Function/functions to seed data
+
+module.exports = { };
