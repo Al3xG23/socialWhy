d8634bc729fc94b4063cf1a2f653a0609f5b894c
Onyx Vega <mabs.24gl@gmail.com>
2024-05-05 00:18:57 -0400

    worked on user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,32 @@
 const { Schema, model } = require('mongoose');
 
 // TODO
+// Schema to create User Model
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            max_length: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            max_length: 50,
+        },
+        reactions: [reactionScheam],
+        thoughts: [thoughtSchema],
+        friends: [friendSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
 
 module.exports = User;
