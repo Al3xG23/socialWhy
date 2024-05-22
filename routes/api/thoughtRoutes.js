4284518814ebbf78b4c2a92ce110335d4e28b264
Onyx Vega <mabs.24gl@gmail.com>
2024-05-22 14:17:14 -0400

    had a couple of plurals on my thoughts-reactions route that needed to be singular

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -22,11 +22,11 @@ router
 .delete(deleteThought);
 
 router
-.route('/:thoughtsId/reactions')
+.route('/:thoughtId/reactions')
 .post(createReaction);
 
 router
-.route('/:thoughtsId/reactions/:reactionId')
+.route('/:thoughtId/reactions/:reactionId')
 .delete(deleteReaction);
 
 module.exports = router;
